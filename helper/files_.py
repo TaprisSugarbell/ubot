eb5501b05a3167c574f74825b260c9e374dbd729
Sugarbell <anonesayu@gmail.com>
2021-07-05 13:29:19 -0600

    Ubot v0.14 | #ADD files_.py

---
+++ helper/files_.py
@@ -0,0 +1,25 @@
+async def file_recognize(filename, out="./"):
+    images = ["jpg", "png", "webp"]
+    videos = ["mp4", "mkv", "webm"]
+    songs = ["mp3", "FLAC", "m4a", "ogg"]
+    documents = ["zip", "rar", "apk"]
+    direcs = {
+        "image": images,
+        "video": videos,
+        "song": songs,
+        "document": documents}
+    try:
+        ext = filename.split(".")[-1]
+        print(ext)
+        if ext in direcs["image"]:
+            file_type = "image"
+        elif ext in direcs["video"]:
+            file_type = "video"
+        elif ext in direcs["song"]:
+            file_type = "song"
+        else:
+            file_type = "document"
+    except:
+        file_type = None
+        ext = None
+    return file_type
