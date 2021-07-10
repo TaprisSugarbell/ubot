6e197f42d7c23f24b038ae4799f43aac2251f1f4
Sugarbell <67601252+taprissugarbell@users.noreply.github.com>
2021-07-09 18:59:32 -0600

    Ubot v0.24 | #IMPROVEMENTS

--- plugins/scrap.py
+++ plugins/scrap.py
@@ -1,6 +1,12 @@
 import os
 import re
+import json
+import random
+import string
+import logging
 import requests
+import urllib.request
+from shutil import rmtree
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 import pyrogram.errors.exceptions
@@ -13,9 +19,45 @@ ADMINS_STR = os.getenv("ADMINS")
 ADMINS = [int(i) for i in ADMINS_STR.split(" ")]
 
 
+async def crfile(client, chat, text, title="links", out="./Downloads/"):
+    file = f"{out}{title}.txt"
+    if not os.path.isdir(out):
+        os.makedirs(out)
+    with open(file, "w") as w:
+        w.write(text)
+    await client.send_document(chat_id=chat,
+                               document=file)
+    rmtree(out)
+
+
+async def ta_s(url):
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, "html.parser")
+    script = soup.find_all("script")[-2]
+    lest = script.contents[0].strip().split(";")[0].split("var videos = ")[1]
+    json_dumps = lest.replace(
+        "[", "{").replace("]", "}").replace("},{", ",").replace(
+        "\\", "").replace('","', '":"').replace(",0", "")[1:-1]
+    json_bruh = json.loads(json_dumps).values()
+    soup = BeautifulSoup(r.content, 'html.parser')
+    lnk = []
+    for i in json_bruh:
+        lnk.append(i)
+    for script in soup.find_all(attrs={"class": "btn btn-success btn-download btn-sm rounded-pill"}):
+        url = script['href']
+        lnk.append(url)
+    return lnk
+
+
 async def scrap(client, message):
+    title = ""
     chat = message["chat"]["id"]
+    command = message["command"][0]
     text = " ".join(message["text"].split(" ")[1:])
+    # Temp Directory
+    key = string.hexdigits
+    session_random = "".join([random.choice(key) for i in range(5)])
+    tmp_directory = f"./Downloads/{message['from_user']['id']}/{session_random}/"
     config = text.split("|")
     url = config[0]
     # if "http" not in url:
@@ -27,15 +69,44 @@ async def scrap(client, message):
         r = requests.get(url).content
     except requests.exceptions.MissingSchema as e:
         index = re.search(r"http[\w]{0,1000}", e.args[0]).start()
-        r = requests.get(e.args[0][index:-1]).content
+        url = e.args[0][index:-1]
+        r = requests.get(url).content
+    try:
+        host = urllib.request.Request(url).host
+    except Exception as e:
+        print(e)
+        host = None
     soup = BeautifulSoup(r, "html.parser")
-    # ht = re.findall(r'"https?://[\s]{0,1000}[\S]{0,1000}"', str(r))
-    # regexz = [i.replace('"', "") for i in ht]
-    a = soup.find_all("a")
-    c = [b.get("href") for b in a]
+    if "f" in command:
+        ht = re.findall(r'"https?://[\s]{0,1000}[\S]{0,1000}"', str(r))
+        alll = [i.replace('"', "") for i in ht]
+        try:
+            if "tioanime" in host:
+                u = await ta_s(url)
+                for i in u:
+                    alll.append(i)
+        except IndexError:
+            logging.info("IndexError, no hay links de capítulos")
+    if "t" in command:
+        alll = []
+        if "tioanime" in host:
+            title = soup.find("h1").text
+            u = await ta_s(url)
+            for i in u:
+                alll.append(i)
+    else:
+        a = soup.find_all("a")
+        alll = [b.get("href") for b in a]
+    c = []
+    for i in alll:
+        if i[0] == "/" and host is not None:
+            c.append(host + i)
+        else:
+            c.append(i)
     regxx = r"{}".format(regex)
     if regex:
         c = [i for i in c if len(re.findall(regxx, i)) > 0]
+
     text = ""
     # for i in regexz:
     #     text += f"{i}\n"
@@ -43,20 +114,18 @@ async def scrap(client, message):
         # if i is not None:
         text += f"{i}\n"
     # print(text)
+    out = tmp_directory
     try:
-        await message.edit(text)
+        if "t" in command:
+            await crfile(client, chat, text, title, out)
+        else:
+            await message.edit(text)
     except pyrogram.errors.MessageEmpty:
         await message.edit("No se encontro nada o hubo un error en la busqueda")
     except pyrogram.errors.MessageTooLong:
-        with open("./Downloads/links.txt", "w") as w:
-            w.write(text)
-        await client.send_document(chat_id=chat,
-                                   document="./Downloads/links.txt")
-        os.unlink("./Downloads/links.txt")
+        await crfile(client, chat, text, out=out)
 
 
-@Client.on_message(filters.user(ADMINS) & filters.command(["scrap", "sc"], COMMANDS))
+@Client.on_message(filters.user(ADMINS) & filters.command(["scrap", "sc", "scrapf", "scf", "sct"], COMMANDS))
 async def scrap_web(client, message):
     await scrap(client, message)
-
-
--- plugins/yt.py
+++ plugins/yt.py
@@ -33,7 +33,7 @@ async def convert(filename):
     return file_
 
 
-async def upload_video(client, chat, tmp_directory, file, thumb):
+async def upload_video(client, chat, tmp_directory, file, thumb, capt=""):
     clip = VideoFileClip(file)
     time_ = int(clip.duration)
     size = clip.size
@@ -45,38 +45,45 @@ async def upload_video(client, chat, tmp_directory, file, thumb):
                                 thumb=f"{tmp_directory}thumb.jpg",
                                 duration=time_,
                                 height=height,
-                                width=width)
+                                width=width,
+                                caption=capt)
     else:
         await client.send_video(chat_id=chat,
                                 video=file,
                                 duration=time_,
                                 height=height,
-                                width=width)
+                                width=width,
+                                caption=capt)
 
 
-async def upload_document(client, chat, tmp_directory, file, thumb):
+async def upload_document(client, chat, tmp_directory, file, thumb, capt=""):
     if thumb:
         await client.send_document(chat_id=chat,
                                    document=file,
-                                   thumb=f"{tmp_directory}thumb.jpg")
+                                   thumb=f"{tmp_directory}thumb.jpg",
+                                   caption=capt)
     else:
         await client.send_document(chat_id=chat,
-                                   document=file)
+                                   document=file,
+                                   caption=capt)
 
 
-async def upload_photo(client, chat, file):
+async def upload_photo(client, chat, file, capt=""):
     await client.send_photo(chat_id=chat,
-                            photo=file)
+                            photo=file,
+                            caption=capt)
 
 
-async def upload_audio(client, chat, tmp_directory, file, thumb):
+async def upload_audio(client, chat, tmp_directory, file, thumb, capt=""):
     if thumb:
         await client.send_audio(chat_id=chat,
                                 audio=file,
-                                thumb=f"{tmp_directory}thumb.jpg")
+                                thumb=f"{tmp_directory}thumb.jpg",
+                                caption=capt)
     else:
         await client.send_audio(chat_id=chat,
-                                audio=file)
+                                audio=file,
+                                caption=capt)
 
 
 @Client.on_message(filters.user(ADMINS) & filters.command(["yt", "ytf", "yta", "ytb"], COMMANDS))
@@ -86,11 +93,23 @@ async def yt(client, message):
     session_random = "".join([random.choice(key) for i in range(5)])
     chat = message["chat"]["id"]
     command = message["command"][0]
-    text = " ".join(message["text"].split(" ")[1:])
+    string_ = " ".join(message["text"].split(" ")[1:])
+    strplit = string_.split("|")
+    text = strplit[0]
     tmp_directory = f"./Downloads/{message['from_user']['id']}/{session_random}/"
     # tmp_directory = f"./Downloads/{message['from_user']['id']}/D76bE/"
     file = await download_file(text, tmp_directory)
     ftype = await file_recognize(file, tmp_directory)
+    if len(strplit) > 1:
+        fil_ename = strplit[1]
+        os.rename(file, fil_ename)
+        file = tmp_directory + fil_ename
+        try:
+            caption = strplit[2]
+        except IndexError:
+            caption = ""
+    else:
+        caption = ""
     try:
         if "thumb.jpg" not in os.listdir(tmp_directory):
             await generate_screen_shots(file, tmp_directory, 300, 1)
@@ -101,27 +120,27 @@ async def yt(client, message):
     try:
         if ftype == "video":
             if command == "ytf":
-                await upload_document(client, chat, tmp_directory, file, thumb)
+                await upload_document(client, chat, tmp_directory, file, thumb, caption)
             elif command == "ytb":
-                await upload_video(client, chat, tmp_directory, file, thumb)
+                await upload_video(client, chat, tmp_directory, file, thumb, caption)
                 await upload_document(client, chat, tmp_directory, file, thumb)
             elif command == "yta":
                 song = await convert(file)
-                await upload_audio(client, chat, tmp_directory, song, thumb)
+                await upload_audio(client, chat, tmp_directory, song, thumb, caption)
             else:
-                await upload_video(client, chat, tmp_directory, file, thumb)
+                await upload_video(client, chat, tmp_directory, file, thumb, caption)
         elif ftype == "image":
             if command == "ytf":
-                await upload_document(client, chat, tmp_directory, file, thumb)
+                await upload_document(client, chat, tmp_directory, file, thumb, caption)
             elif command == "ytb":
-                await upload_photo(client, chat, file)
+                await upload_photo(client, chat, file, caption)
                 await upload_document(client, chat, tmp_directory, file, thumb)
             else:
-                await upload_photo(client, chat, file)
+                await upload_photo(client, chat, file, caption)
         elif ftype == "song":
-            await upload_audio(client, chat, tmp_directory, file, thumb)
+            await upload_audio(client, chat, tmp_directory, file, thumb, caption)
         else:
-            await upload_document(client, chat, tmp_directory, file, thumb)
+            await upload_document(client, chat, tmp_directory, file, thumb, caption)
         rmtree(tmp_directory)
     except Exception as e:
         rmtree(tmp_directory)
